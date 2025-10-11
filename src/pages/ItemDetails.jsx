b95aeea2a5af05c9d40aa293ef696b957df1dde3
motelope101-dotcom <motelope101@gmail.com>
2025-10-10 21:30:59 -0400

    -REVISED-  ITEMDETAILS.JSX

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,13 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 import EthImage from "../images/ethereum.svg";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
 
 const ItemDetails = () => {
+  const { nftId } = useParams();
+  const [nft, setNft] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+
+    const fetchNft = async () => {
+      try {
+        const res = await fetch(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${nftId}`
+        );
+        const data = await res.json();
+        setNft(data);
+      } catch (err) {
+        console.error("Error fetching NFT:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNft();
+  }, [nftId]);
+
+  if (loading) {
+    return (
+      <div className="container text-center mt-5">
+        <div className="skeleton-loader">Loading NFT details...</div>
+      </div>
+    );
+  }
+
+  if (!nft) {
+    return (
+      <div className="container text-center mt-5">
+        <p className="text-danger">NFT not found.</p>
+      </div>
+    );
+  }
 
   return (
     <div id="wrapper">
@@ -18,67 +52,64 @@ const ItemDetails = () => {
             <div className="row">
               <div className="col-md-6 text-center">
                 <img
-                  src={nftImage}
+                  src={nft.nftImage}
                   className="img-fluid img-rounded mb-sm-30 nft-image"
-                  alt=""
+                  alt={nft.title}
                 />
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>Rainbow Style #194</h2>
+                  <h2>{nft.title}</h2>
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
-                      <i className="fa fa-eye"></i>
-                      100
+                      <i className="fa fa-eye"></i> {nft.views}
                     </div>
                     <div className="item_info_like">
-                      <i className="fa fa-heart"></i>
-                      74
+                      <i className="fa fa-heart"></i> {nft.likes}
                     </div>
                   </div>
-                  <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
-                  </p>
+
+                  <p>{nft.description}</p>
+
                   <div className="d-flex flex-row">
                     <div className="mr40">
                       <h6>Owner</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                          <Link to={`/author/${nft.ownerId}`}>
+                            <img className="lazy" src={nft.ownerImage} alt={nft.ownerName} />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to={`/author/${nft.ownerId}`}>{nft.ownerName}</Link>
                         </div>
                       </div>
                     </div>
-                    <div></div>
                   </div>
+
                   <div className="de_tab tab_simple">
                     <div className="de_tab_content">
                       <h6>Creator</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                          <Link to={`/author/${nft.creatorId}`}>
+                            <img className="lazy" src={nft.creatorImage} alt={nft.creatorName} />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to={`/author/${nft.creatorId}`}>{nft.creatorName}</Link>
                         </div>
                       </div>
                     </div>
+
                     <div className="spacer-40"></div>
                     <h6>Price</h6>
                     <div className="nft-item-price">
-                      <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                      <img src={EthImage} alt="ETH" />
+                      <span>{nft.price}</span>
                     </div>
                   </div>
                 </div>
